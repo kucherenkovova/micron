59d6c93a1ab1c576d10a01398dd38f8ab2b12c86
kucherenkovova <kucherenkovova@gmail.com>
2023-05-09 16:12:32 +0300

    Protect OnPanic with mutex. (#7)

--- panic.go
+++ panic.go
@@ -4,5 +4,7 @@ package micron
 // Usually used to notify an external system (slack/email/sentry/etc) about unhandled issue in the running app.
 // App instance can have only one panic callback. Every OnPanic invocation overwrites previously registered handler.
 func (a *App) OnPanic(f func(any)) {
+	a.mu.Lock()
 	a.panicHandler = f
+	a.mu.Unlock()
 }
